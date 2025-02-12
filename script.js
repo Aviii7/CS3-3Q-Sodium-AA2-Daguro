fedf76ba5d95cfd4065e364d09b5689f03a8592f
Aviii7 <jacdaguro22@src.pshs.edu.ph>
2025-02-12 13:36:48 +0800

    Update script.js

--- script.js
+++ script.js
@@ -2,12 +2,12 @@ function myFunc() {
   let a = prompt("Enter value for the initial population: ");
   let b = prompt("Enter value for the rate of growth: ");
   let c = prompt("Enter value for the time elapsed in hours: ");
-  let d = parseFloat(a) * ( Math.E ** (parseFloat(b) * parseFloat(c)));
+  let d = parseFloat(a) * Math.E ** (parseFloat(b) * parseFloat(c));
   d = Math.round(d);
 
   let e = prompt("Enter the region/location of the monster: ");
   let f = prompt("Enter the name of the monster: ");
-  let g = concat(e, " ", f);
+  let g = e.concat(" ", f);
   g = toUpperCase(g);
 
   document.getElementById('result').innerhtml = "After " + c + " hours, the population of " + g + " has increased to " + d + "!";
