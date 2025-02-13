c3bad9985a38149d2c6bee912fdbc25b755d1beb
Aviii7 <jacdaguro22@src.pshs.edu.ph>
2025-02-13 18:37:35 +0800

    Update script.js

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ function myFunc() {
 
   let locationOfMonster = prompt("Enter the region/location of the monster: ");
   let nameOfMonster = prompt("Enter the name of the monster: ");
-  let mergedString = concat(locationOfMonster, " ", nameOfMonster);
+  let mergedString = locationOfMonster.concat(" ", nameOfMonster);
   mergedString = mergedString.toUpperCase();
 
   document.getElementById('result').innerHTML = 'After ' + elapsedHours + ' hours, the population of ' + mergedString + ' has increased to ' + calculations + '!';
