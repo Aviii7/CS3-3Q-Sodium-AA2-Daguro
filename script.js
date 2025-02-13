cc34b2ff86b4bc0dbded8727d9d7da9bcc05daa6
Aviii7 <jacdaguro22@src.pshs.edu.ph>
2025-02-13 11:27:08 +0800

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function myFunc() {
   let a = prompt("Enter value for the initial population: ");
   let b = prompt("Enter value for the rate of growth: ");
   let c = prompt("Enter value for the time elapsed in hours: ");
-  let d = parseFloat(a) * Math.E ** (parseFloat(b) * parseFloat(c));
+  let d = parseFloat(a) * Math.pow(Math.E, (parseFloat(b) * parseFloat(c)));
   d = Math.round(d);
 
   let e = prompt("Enter the region/location of the monster: ");
